40bbb614d3092fbc67d40d331eec9cdcc6428175
beagbarreto <33846968+Beagbarreto@users.noreply.github.com>
2021-02-17 15:33:34 -0600

    Search and filter implemented Used useEffect in order to get pokemons and map through them. Filtered pokemons in app component and passed them as props after filtered to pokemon Item component.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CardItem from "./components/CardItem";
 import { pokemons } from "./mock/mockedData";
 
 import "./styles.css";
 
 export default function App() {
-  const [data, setData] = useState("");
+  const [pokemonsData, setPokemonsData] = useState([]);
+  const [loading, SetLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [filterPokemons, setFilterPokemons] = useState([]);
 
-  const handleChange = (e) => {
-    setData(e.targe.value);
-  };
+  // useEffect(() => {
+  //   const fetchData = () => {
+  //     const pokemons = mockPokemonsData;
+  //     pokemons.map((poke, i) => {
+  //       setPokemons(poke, i);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(data);
-  };
+  //     });
+  //     setPokemons(pokemons);
+  //   };
+  //   fetchData();
+  // }, []);
+  const pokemonDetail = { pokemons };
+  console.log(pokemonDetail);
+
+  useEffect(() => {
+    SetLoading(true);
+    setPokemonsData(pokemons);
+    console.log(pokemons);
+    SetLoading(false);
+  }, []);
+
+  useEffect(() => {
+    setFilterPokemons(
+      pokemons.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, pokemons]);
+
+  if (loading) {
+    return <p>Loading pokemons...</p>;
+  }
 
   return (
     <div className="app">
       <h2>Mini Challenge 3: Poke filter</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          onChange={handleChange}
-          value={data}
-          placeholder="What Pokemon are you looking for?"
-        />
-      </form>
+      <input
+        type="text"
+        onChange={(e) => setSearch(e.target.value)}
+        // value={pokemons}
+        placeholder="What Pokemon are you looking for?"
+      />
       <div className="cards">
-        {/**
-        Filter pokemons data and map them to return an array of CardItems.
-         */}
+        {filterPokemons.map((pokemon, index) => (
+          <CardItem key={index} {...pokemon} />
+        ))}
       </div>
     </div>
   );
--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function CardItem({ name, sprite }) {
+const CardItem = (props) => {
+  const { name, sprite } = props;
+
   return (
     <div className="card-item">
+      <h1>POKEMON</h1>
       <h2>{name}</h2>
       <img src={sprite} alt="pokemon" />
     </div>
   );
-}
+};
 
 export default CardItem;
